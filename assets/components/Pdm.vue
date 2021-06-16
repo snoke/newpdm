24f2690c16b3f02d42c3c110aa1d9e790b2755f4
unknown <stowwel@gmail.com>
2021-06-16 06:53:08 +0200

    removing text-deco from main elements

--- assets/components/Pdm.vue
+++ assets/components/Pdm.vue
@@ -2,19 +2,19 @@
   <div style="height:100%;">
     <div class="container" style="height:100%;">
       <div class="row">
-        <router-link  to="/Umzug"  class="col d-flex justify-content-center pt-2" >
+        <router-link  to="/Umzug"  class="pdm_link col d-flex justify-content-center pt-2" >
           <Card 
             title="Umzug"
             text="Umzug"
             icon="truck-loading" />
         </router-link>
-        <router-link  to="/Transport" class="col d-flex justify-content-center pt-2">
+        <router-link  to="/Transport" class="pdm_link col d-flex justify-content-center pt-2">
           <Card 
             title="Transport"
             text="Transport"
             icon="shipping-fast"/>
         </router-link>
-        <router-link  to="/Entsorgung" class="col d-flex justify-content-center pt-2">
+        <router-link  to="/Entsorgung" class="pdm_link col d-flex justify-content-center pt-2">
           <Card 
             title="Entsorgung"
             text="Entsorgung"
