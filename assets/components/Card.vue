9fb07d3fae47d8d66ebcab12fdb9e2b8e97d3867
unknown <stowwel@gmail.com>
2021-06-16 08:30:51 +0200

    fixing scrolling

--- assets/components/Card.vue
+++ assets/components/Card.vue
@@ -2,7 +2,7 @@
     <div class="card col-lg shadow">
         <div class="card-body">
             <i style="float:right;" :class='"fas fa-" + this.icon'></i>
-            <h2 class="card-title" style="font-size:1.6rem;">{{title}}</h2>
+            <h2 class="card-title  text-center " style="font-size:1.6rem;">{{title}}</h2>
             <p class="row card-text">{{text}}</p>
         </div>
     </div>
