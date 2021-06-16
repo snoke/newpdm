bc7fcb90e59de07031a2b78cb2232d30e60b1a17
unknown <stowwel@gmail.com>
2021-06-16 22:04:27 +0200

    styling...

--- assets/components/Footer.vue
+++ assets/components/Footer.vue
@@ -30,14 +30,14 @@
         </div>
         <div class="row d-flex justify-content-center">
             <div class="col-md d-flex justify-content-center" >
-                <div class=" pt-1 w-50" style="border-bottom:1px solid #CCC;">
+                <div class=" pt-1 w-75" style="border-bottom:1px solid #CCC;">
                 </div>
             </div>
         </div>
         <div class="row d-flex justify-content-center">
             <div class="col-md d-flex justify-content-center">
                 <div class="card-body text-center" ><i style=";" class="fas fa-copyright"></i> code & design
-                    <p class="row card-text text-center"  style="font-size:0.9rem;"><a href="https://www.stefan-sander.online" target="_blank">stefan-sander</a></p>
+                    <p class="row card-text text-center"  style="font-size:0.9rem;"><a href="https://www.stefan-sander.online" target="_blank">Stefan Sander</a></p>
                 </div>
             </div>
         </div>
