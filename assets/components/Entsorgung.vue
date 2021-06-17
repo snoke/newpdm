9749e44b95083bf4b4eb8c8fe39a4bc95eadd9f3
unknown <stowwel@gmail.com>
2021-06-17 15:49:40 +0200

    styling...

--- assets/components/Entsorgung.vue
+++ assets/components/Entsorgung.vue
@@ -6,7 +6,7 @@
                   <router-link  class="nav-link" to="/">
                   <i style="float:left;" :class='"mt-2 fas fa-arrow-left  text-primary"'></i></router-link>
           <i style="float:right;" :class='"fas fa-dumpster  text-primary"'></i>
-            <h2 class="card-title text-center w-100 pr-5 text-shadow-sm" style="font-size:1.8rem;">Entsorgung</h2>
+            <h2 class="card-title text-center w-100 pr-4 text-shadow-sm" style="font-size:1.8rem;">Entsorgung</h2>
             
           <p class="row card-text p-3 text-dark">Entsorgung</p>
         </div>
